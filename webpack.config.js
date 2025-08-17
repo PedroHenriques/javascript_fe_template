a9da9d3439673a30ea4d1bc1cb30ef10cdc2112c
Pedro Henriques <pedro@pedrojhenriques.com>
2025-08-17 13:54:19 +0000

    more boilerplating

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const getConfigs = require('./configLoader.js');
+
+const configs = getConfigs();
+
+const makeScssRule = (isProd, baseUrl) => {
+  const use = [
+    { loader: 'css-loader', options: { url: false, sourceMap: !isProd } },
+    {
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: !isProd,
+        postcssOptions: { plugins: [require('autoprefixer')()] },
+      }
+    },
+    {
+      loader: 'sass-loader',
+      options: {
+        sourceMap: !isProd,
+        additionalData: `$baseUrl: "${baseUrl}";`,
+      }
+    }
+  ];
+  
+  // dev: style-loader for HMR; prod: extract CSS
+  return {
+    test: /\.s?[ac]ss$/,
+    use: [isProd ? MiniCssExtractPlugin.loader : 'style-loader', ...use]
+  };
+};
+
+module.exports = (env = {}, argv = {}) => {
+  const isProd = argv.mode === 'production';
+  const baseUrl = isProd ? '/games/minesweeper-v2/' : '/';
+
+  return {
+    mode: isProd ? 'production' : 'development',
+    entry: ['whatwg-fetch', './src/ts/index.tsx'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: baseUrl,
+      filename: isProd ? 'js/[name].[contenthash:8].js' : 'js/bundle.js',
+      chunkFilename: isProd ? 'js/[name].[contenthash:8].js' : 'js/[name].js',
+      clean: true,
+    },
+    cache: { type: 'filesystem' },
+    resolve: { extensions: ['.tsx', '.ts', '.js'] },
+    module: {
+      rules: [
+        { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+        makeScssRule(isProd, baseUrl),
+        {
+          test: /\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$/i,
+          type: 'asset',
+          generator: { filename: 'assets/[name].[contenthash:8][ext][query]' },
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: 'body',
+        template: path.resolve(__dirname, 'src', 'index.template.html'),
+        filename: 'index.html',
+        favicon: path.resolve(__dirname, 'src', 'favicon.png'),
+        templateParameters: {
+          PAGE_TITLE: configs.PAGE_TITLE,
+        },
+      }),
+      ...(
+        isProd
+        ? [new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash:8].css' })]
+        : []
+      ),
+      new webpack.DefinePlugin({
+        BASE_URL: JSON.stringify(baseUrl),
+      }),
+      new webpack.WatchIgnorePlugin({ paths: [/\.js$/, /\.d\.ts$/] })
+    ],
+    optimization: isProd
+      ? { minimizer: ['...', new CssMinimizerPlugin()], splitChunks: { chunks: 'all' }, runtimeChunk: 'single' }
+      : undefined,
+    devtool: isProd ? 'source-map' : 'inline-source-map',
+    devServer: isProd
+      ? undefined
+      : {
+          port: 3000,
+          open: true,
+          hot: true,
+          historyApiFallback: true,
+        },
+  };
+};
