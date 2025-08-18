7e558ba706183beb54d1dfc912df9d8afe19a721
Pedro Henriques <pedro@pedrojhenriques.com>
2025-08-18 13:25:05 +0000

    Change boilerplate app to use redux.

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import './styles/app.scss';
+import { store } from '@app/store/store';
+import { Provider } from 'react-redux';
+import App from '@app/components/App';
 
 declare const BASE_URL: string;
 console.log('BASE_URL =', BASE_URL);
@@ -8,4 +9,9 @@ console.log('BASE_URL =', BASE_URL);
 const container = document.getElementById('app-container');
 if (!container) { throw new Error('Missing #app-container'); }
 
-createRoot(container).render(<App />);
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
