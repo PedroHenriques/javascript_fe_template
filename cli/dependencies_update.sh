168916e3fcb2f837702984b01b5d9a13fa61c7c0
Pedro Henriques <pedro@pedrojhenriques.com>
2025-08-18 17:14:14 +0000

    Update cli/dependencies_update.sh script.

--- cli/dependencies_update.sh
+++ cli/dependencies_update.sh
@@ -2,14 +2,13 @@
 set -e;
 
 UPDATE=0;
-UPDATE_PROMPT=1;
 USE_DOCKER=0;
 RUNNING_IN_PIPELINE=0;
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
     -u|--update) UPDATE=1; shift 1;;
-    -y) UPDATE_PROMPT=0; shift 1;;
+    -y) shift 1;;
     --docker) USE_DOCKER=1; shift 1;;
     --cicd) RUNNING_IN_PIPELINE=1; USE_DOCKER=1; shift 1;;
 
@@ -19,20 +18,17 @@ done
 
 OPTS="";
 if [ $UPDATE -eq 1 ]; then
-  OPTS="${OPTS} -u";
-  if [ $UPDATE_PROMPT -eq 1 ]; then
-    OPTS="${OPTS}:Prompt";
-  fi
+  OPTS="${OPTS} -u && npm i";
 fi
 
-CMD="dotnet tool restore; dotnet outdated ${OPTS} ./*.sln";
+CMD="npm run versionbump -- --workspaces --root ${OPTS}";
 if [ $USE_DOCKER -eq 1 ]; then
   INTERACTIVE_FLAGS="-it";
   if [ $RUNNING_IN_PIPELINE -eq 1 ]; then
     INTERACTIVE_FLAGS="-i";
   fi
 
-  docker run --rm ${INTERACTIVE_FLAGS} -v "./:/app/" -w "/app/" mcr.microsoft.com/dotnet/sdk:8.0-noble /bin/sh -c "${CMD}";
+  docker run --rm ${INTERACTIVE_FLAGS} -v "./:/app/" -w "/app/" node:24-bullseye-slim /bin/sh -c "${CMD}";
 else
   eval "${CMD}";
 fi
