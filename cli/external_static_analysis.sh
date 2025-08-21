ec10da8e5d4be07444c4599656c533fea4b0ca37
Pedro Henriques <pedro@pedrojhenriques.com>
2025-08-21 09:20:37 +0000

    Update sonar integration.

--- cli/external_static_analysis.sh
+++ cli/external_static_analysis.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e;
 
+: "${TEST_COVERAGE_DIR_PATH:=coverageReport}";
+: "${TEST_COVERAGE_FILE_NAME:=lcov.info}";
+
 USE_DOCKER=0;
 RUNNING_IN_PIPELINE=0;
 
@@ -15,9 +18,20 @@ done
 
 rm -rf .sonarqube;
 
-TEST_COVERAGE_PATH="./test/**/${TEST_COVERAGE_FILE_NAME}";
-CMD="dotnet tool restore && dotnet sonarscanner begin /k:"${EXTERNAL_STATIC_ANALYSIS_PROJ_KEY}" /o:"${EXTERNAL_STATIC_ANALYSIS_ORG}" /d:sonar.token="${EXTERNAL_STATIC_ANALYSIS_TOKEN}" /d:sonar.host.url="${EXTERNAL_STATIC_ANALYSIS_HOST}" /d:sonar.cs.opencover.reportsPaths="${TEST_COVERAGE_PATH}" /d:sonar.projectBaseDir=/app /d:sonar.exclusions=**/bin/**,**/obj/**,setup/**,app/setup/** /d:sonar.coverage.exclusions=setup/**,app/setup/** && dotnet build && chmod +x ./cli/test.sh && ./cli/test.sh --coverage && dotnet sonarscanner end /d:sonar.token="${EXTERNAL_STATIC_ANALYSIS_TOKEN}"";
-echo "${CMD}";
+if [ ! -f "${TEST_COVERAGE_DIR_PATH}/${TEST_COVERAGE_FILE_NAME}" ]; then
+  echo "Coverage not found at ${TEST_COVERAGE_DIR_PATH}/${TEST_COVERAGE_FILE_NAME} — generating…";
+  
+  FLAGS="";
+  if [ $RUNNING_IN_PIPELINE -eq 1 ]; then
+    FLAGS="--cicd";
+  elif [ $USE_DOCKER -eq 1 ]; then
+    FLAGS="--docker";
+  fi
+  
+  TEST_COVERAGE_DIR_PATH="${TEST_COVERAGE_DIR_PATH}" TEST_COVERAGE_FILE_NAME="${TEST_COVERAGE_FILE_NAME}" sh cli/coverage.sh ${FLAGS};
+fi
+
+CMD="sonar-scanner";
 
 if [ $USE_DOCKER -eq 1 ]; then
   INTERACTIVE_FLAGS="-it";
@@ -26,7 +40,8 @@ if [ $USE_DOCKER -eq 1 ]; then
   fi
 
   docker network create myapp_shared || true;
-  docker run --rm ${INTERACTIVE_FLAGS} -v "./:/app/" -w "/app/" mcr.microsoft.com/dotnet/sdk:8.0-noble /bin/sh -c "${CMD}";
+  docker run --rm ${INTERACTIVE_FLAGS} -v "./:/app/" -w "/app/" -e TEST_COVERAGE_DIR_PATH -e TEST_COVERAGE_FILE_NAME -e EXTERNAL_STATIC_ANALYSIS_PROJ_KEY -e EXTERNAL_STATIC_ANALYSIS_ORG -e EXTERNAL_STATIC_ANALYSIS_HOST -e SONAR_TOKEN="${EXTERNAL_STATIC_ANALYSIS_TOKEN}" sonarsource/sonar-scanner-cli:latest /bin/sh -c "${CMD}";
 else
+  export TEST_COVERAGE_DIR_PATH TEST_COVERAGE_FILE_NAME;
   eval "${CMD}";
 fi
